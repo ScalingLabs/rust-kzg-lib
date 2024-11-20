d0a571fbac4186c0b11458149b02b195d3ccafd6
Owanikin <oderindeife@gmail.com>
2024-11-20 16:01:07 +0100

    load trusted setup string

--- eip4844/src/eip_4844.rs
+++ eip4844/src/eip_4844.rs
@@ -2,7 +2,8 @@
 extern crate alloc;
 
 use core::ffi::c_uint;
-use blst::{blst_fr, blst_p1, blst_p2};
+use blst::{blst_fr, blst_p1, blst_p2, byte};
+use sha2::{Digest, Sha256};
 
 use crate::utils::bit_reversal_permutation;
 
@@ -10,7 +11,8 @@ use crate::utils::bit_reversal_permutation;
 pub const FIELD_ELEMENTS_PER_BLOB: usize = 4096;
 pub const BYTES_PER_FIELD_ELEMENT: usize = 32;
 pub const BYTES_PER_BLOB: usize = BYTES_PER_FIELD_ELEMENT * FIELD_ELEMENTS_PER_BLOB;
-pub const BYTES_PER_G1: usize = 96;
+pub const BYTES_PER_G1: usize = 48;
+pub const BYTES_PER_G2: usize = 96;
 pub const BYTES_PER_PROOF: usize = 48;
 pub const BYTES_PER_COMMITMENT: usize = 48;
 
@@ -67,3 +69,61 @@ pub struct CKZSettings {
     pub g2_values: *mut blst_p2,
 }
 
+
+pub fn load_trusted_setup_string(contents: &str) -> Result<(Vec<u8>, Vec<u8>), String> {
+    let mut offset = 0;
+
+    const TRUSTED_SETUP_ERROR: &str = "Incorrect trusted setup format";
+
+    #[inline(always)]
+    fn scan_number(offset: &mut usize, contents: &str) -> Result<usize, String> {
+        *offset += contents[(*offset)..].find(|c: char| !c.is_whitespace()).ok_or_else(|| String::from(TRUSTED_SETUP_ERROR))?;
+        let start = *offset;
+        *offset += contents[(*offset)..].find(|c: char| !c.is_ascii_digit()).ok_or_else(|| String::from(TRUSTED_SETUP_ERROR))?;
+        let end = *offset;
+        contents[start..end].parse::<usize>().map_err(|_| String::from(TRUSTED_SETUP_ERROR))
+    }
+
+    let g1_point_count = scan_number(& mut offset, contents)?;
+
+    // FixME: must be TRUSTED_SETUP_NUM_G1_POINTS
+    if g1_point_count != TRUSTED_SETUP_NUM_G1_POINTS {
+        return Err(String::from(TRUSTED_SETUP_ERROR));
+    }
+
+    let g2_point_count = scan_number(&mut offset, contents)?;
+
+    if g2_point_count != TRUSTED_SETUP_NUM_G2_POINTS {
+        return Err(String::from(TRUSTED_SETUP_ERROR));
+    }
+
+    let mut g1_bytes = vec![0u8; g1_point_count * BYTES_PER_G1];
+    let mut g2_bytes = vec![0u8; g1_point_count * BYTES_PER_G2];
+
+    #[inline(always)]
+    fn scan_hex_byte(offset: &mut usize, contents: &str) -> Result<u8, String> {
+        *offset += contents[(*offset)..].find(|c: char| !c.is_whitespace()).ok_or_else(|| String::from(TRUSTED_SETUP_ERROR))?;
+        let start = *offset;
+        let end = if contents.get((*offset + 1)..).map(|it| {
+            it.chars().next().map(|c| c.is_ascii_hexdigit()).unwrap_or(false)
+        }).unwrap_or(false)
+        {
+            *offset += 2;
+            *offset
+        } else {
+            *offset += 1;
+            *offset
+        };
+        u8::from_str_radix(&contents[start..end], 16).map_err(|_| String::from(TRUSTED_SETUP_ERROR))
+    }
+
+    for byte in &mut g1_bytes {
+        *byte = scan_hex_byte(&mut offset, contents)?
+    }
+
+    for byte in &mut g2_bytes {
+        *byte = scan_hex_byte(&mut offset, contents)?
+    }
+
+    Ok((g1_bytes, g2_bytes))
+}
